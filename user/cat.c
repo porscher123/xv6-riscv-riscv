5cea9cfea058680f9f29a77a3561685bed90be10
Mi    tiepinuanhu <13236388+mi-tiepinuanhu@user.noreply.gitee.com>
2024-06-24 16:17:17 +0800

    123

--- user/cat.c
+++ user/cat.c
@@ -4,42 +4,46 @@
 
 char buf[512];
 
-void
-cat(int fd)
+void cat(int fd)
 {
-  int n;
+	int n;
 
-  while((n = read(fd, buf, sizeof(buf))) > 0) {
-	// read from the fd and write to the standard output(1) of the cat process
-	if (write(1, buf, n) != n) {
-	  fprintf(2, "cat: write error\n");
-	  exit(1);
+	while ((n = read(fd, buf, sizeof(buf))) > 0)
+	{
+		// read from the fd and write to the standard output(1) of the cat process
+		if (write(1, buf, n) != n)
+		{
+			fprintf(2, "cat: write error\n");
+			exit(1);
+		}
 	}
-  }
-  if(n < 0){
-	fprintf(2, "cat: read error\n");
-	exit(1);
-  }
+	if (n < 0)
+	{
+		fprintf(2, "cat: read error\n");
+		exit(1);
+	}
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  int fd, i;
+	int fd, i;
 
-  if(argc <= 1){
-	cat(0);
-	exit(0);
-  }
-  // for all files, open it, get fd and  call cat(fd)
-  for(i = 1; i < argc; i++){
-	if((fd = open(argv[i], 0)) < 0){
-	  fprintf(2, "cat: cannot open %s\n", argv[i]);
-	  exit(1);
+	if (argc <= 1)
+	{
+		cat(0);
+		exit(0);
 	}
-	cat(fd);
-	// release the file descriptor
-	close(fd);
-  }
-  exit(0);
+	// for all files, open it, get fd and  call cat(fd)
+	for (i = 1; i < argc; i++)
+	{
+		if ((fd = open(argv[i], 0)) < 0)
+		{
+			fprintf(2, "cat: cannot open %s\n", argv[i]);
+			exit(1);
+		}
+		cat(fd);
+		// release the file descriptor
+		close(fd);
+	}
+	exit(0);
 }
